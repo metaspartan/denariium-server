cad33f6247382b25a888ca373f8d6da307ea5352
carsenk <carsenk@gmail.com>
2017-10-23 16:03:38 -0400

    DNR Port

---
+++ build/lib.linux-x86_64-2.7/electrumserver/networks.py
@@ -0,0 +1,14 @@
+# Main network and testnet definitions
+
+params = {
+    'bitcoin_main': {
+        'pubkey_address': 30,
+        'script_address': 90,
+        'genesis_hash': '00000d5dbbda01621cfc16bbc1f9bf3264d641a5dbf0de89fd0182c2c4828fcd'
+    },
+    'bitcoin_test': {
+        'pubkey_address': 18,
+        'script_address': 116,
+        'genesis_hash': '000086bfe8264d241f7f8e5393f747784b8ca2aa98bdd066278d590462a4fdb4'
+    }
+}
